28c8d590fa4b71669ce2813b576185c6c8b0f33b
Subhankar Das <94730919+SubhankarDas2000@users.noreply.github.com>
2022-12-12 15:45:10 +0530

    Create windows_logo.py

---
+++ windows_logo.py
@@ -0,0 +1,31 @@
+from turtle import *
+speed(1)
+width(4)
+bgcolor('black')
+color('blue')
+begin_fill()
+penup()
+goto(-50,60)
+pendown()
+goto (100,100)
+goto (100,-100)
+goto(-50,-60)
+goto(-50,60)
+
+end_fill()
+penup()
+
+color('black')
+width(10)
+
+goto(-50,0)
+pendown()
+goto(100, 0)
+
+penup()
+
+goto (25,80)
+pendown()
+goto(25,-80)
+
+done()
